d39e2828ddd892b87bedf1732bdc0e56ea238553
Cauã <113377429+cauaraphael@users.noreply.github.com>
2024-11-09 21:01:28 -0300

    Create beecrowd1006.py

---
+++ beecrowd1006.py
@@ -0,0 +1,5 @@
+numero_flutuanteA = float(input())
+numero_flutuanteB = float(input())
+numero_flutuanteC = float(input())
+media = ((numero_flutuanteA*2)+(numero_flutuanteB*3)+(numero_flutuanteC*5))/10
+print('MEDIA = %.1f'%media)
