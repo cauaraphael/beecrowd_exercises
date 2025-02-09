a046735b8f7373d5a769cc4fb1b0cb3b08ce3e93
Cauã <113377429+cauaraphael@users.noreply.github.com>
2025-02-09 18:33:02 -0300

    beecrowd2617.sql

---
+++ beecrowd2617.sql
@@ -0,0 +1,9 @@
+SELECT
+        products.name,
+        providers.name
+FROM
+        providers
+        INNER JOIN
+        products ON id_providers = providers.id
+WHERE
+        providers.name = 'Ajax SA'
